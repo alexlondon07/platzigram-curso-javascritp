2685b6a57623cb356ae72ce5b1bc9d9cf606c9b6
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2017-02-12 14:08:12 -0500

    Agregando imagenes del usuarios a timeline

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -6,5 +6,26 @@ var title = require('title');
 page('/', function(ctx, next){
     title('Platzigram');
     var main = document.getElementById('main-container');
-   empty(main).appendChild(template);
+    var pictures =[
+        {
+            user: {
+                username: 'alexlondon07',
+                avatar: 'https://scontent.feoh1-1.fna.fbcdn.net/v/t31.0-8/14853272_10211262828146142_5259747687663940265_o.jpg?oh=366e9489a6cdec1b2decd4092a9394fc&oe=5934E318',
+            },
+            url: 'office.jpg',
+            likes: 200,
+            liked: true
+        },
+        {
+            user: {
+                username: 'alexlondon07',
+                avatar: 'https://scontent.feoh1-1.fna.fbcdn.net/v/t31.0-8/14853272_10211262828146142_5259747687663940265_o.jpg?oh=366e9489a6cdec1b2decd4092a9394fc&oe=5934E318',
+            },
+            url: 'office.jpg',
+            likes: 3000,
+            liked: true
+        },
+    ];
+    
+    empty(main).appendChild(template(pictures));
 })
